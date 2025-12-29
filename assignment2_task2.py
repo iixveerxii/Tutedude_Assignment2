2dd12d9224a5372afe93c83158be49054c73486d
iixveerxii <141078676+iixveerxii@users.noreply.github.com>
2025-12-29 05:49:11 +0530

    Avoiding inbuilt functions

--- assignment2_task2.py
+++ assignment2_task2.py
@@ -1,6 +1,7 @@
-numbers = []
+numbers = 0
 
 for i in range(1,51):
-    numbers.append(i)
-print(f"sum of numbers from 1 to 50 is: {sum(numbers)}")
+    numbers += i
+print(f"sum of numbers from 1 to 50 is: {numbers}")
+
 
